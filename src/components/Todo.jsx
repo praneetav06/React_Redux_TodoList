856b48c2a21c8f66994c5e1e99a506168915d4d9
Praneeta Verma <praneetav06@gmail.com>
2025-04-02 17:41:28 +0530

    removed styling for mark as done feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,31 +4,35 @@ import { useDispatch } from "react-redux";
 import { deleteTodo } from "../features/todo/todoSlice";
 import { markAsDone } from "../features/todo/todoSlice";
 export default function Todo() {
-    const todos = useSelector((state) => state.todos);
-    console.log(todos);
-    const dispatch = useDispatch();
-    const clickHandler = (id) => {
-        console.log("delete", id);
-        dispatch(deleteTodo(id));
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+  const dispatch = useDispatch();
+  const clickHandler = (id) => {
+    console.log("delete", id);
+    dispatch(deleteTodo(id));
+  };
+  const markAsDoneHandler = (id) => {
+    console.log("Marked As Done!", id);
+    if (todos.isDone === true) {
+      dispatch(markAsDone(id));
     }
-    const markAsDoneHandler = (id) => {
-        console.log("Marked As Done!", id);
-        if (todos.isDone === true) {
-            dispatch(markAsDone(id));
-        }
-    }
-    return (
-        <>
-            <AddForm/>
-            <h2>Todo List App</h2>
-            <ul>
-                {todos.map((todo) => (
-                    <li key={todo.id}>{todo.task} &nbsp;&nbsp;<button onClick={() => clickHandler(todo.id)}>Delete</button>&nbsp;&nbsp;
-                    <button style={{
-                        textDecoration: todo.isDone === true ? "color" : "green"}} onClick={() => markAsDoneHandler(todo.id)}>Mark As Done</button>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+  };
+  return (
+    <>
+      <AddForm />
+      <h2>Todo List App</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task} &nbsp;&nbsp;
+            <button onClick={() => clickHandler(todo.id)}>Delete</button>
+            &nbsp;&nbsp;
+            <button onClick={() => markAsDoneHandler(todo.id)}>
+              Mark As Done
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
