e29d9eea1250c34e81abf366189c6c1423dcee15
Praneeta Verma <praneetav06@gmail.com>
2025-04-02 18:39:25 +0530

    fixing mark as done feature

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -3,20 +3,21 @@ import { useDispatch } from "react-redux";
 import { addTodo } from "../features/todo/todoSlice";
 
 export default function AddForm() {
-    const [task, setTask] = useState("");
-    const dispatch = useDispatch();
+  const [task, setTask] = useState("");
+  const dispatch = useDispatch();
 
-    const submitHandler = (evt) => {
-        evt.preventDefault();
-        console.log(task);
-        dispatch(addTodo(task));
-        setTask("");
-    }
+  const submitHandler = (evt) => {
+    evt.preventDefault();
+    console.log(task);
+    dispatch(addTodo(task));
+    setTask(" ");
+  };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <input type="text" onChange={(e) => setTask(e.target.value)}/>&nbsp;&nbsp;
-            <button>Add Task</button>
-        </form>
-    )
+  return (
+    <form onSubmit={submitHandler}>
+      <input type="text" onChange={(e) => setTask(e.target.value)} />
+      &nbsp;&nbsp;
+      <button>Add Task</button>
+    </form>
+  );
 }
