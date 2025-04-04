050386e9f284aca6bad2ee6c26354c3d2de57cb3
Praneeta Verma <praneetav06@gmail.com>
2025-04-04 15:29:04 +0530

    fixed file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import Todo from './components/Todo';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import "./App.css";
+import Todo from "./components/Todo";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
-
   return (
     <>
       <Provider store={store}>
-        <Todo/>
+        <Todo />
       </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
